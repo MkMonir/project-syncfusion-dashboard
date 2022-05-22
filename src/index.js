2eea0f6bdc0b7cf69a858be08f0e573792151e79
MkMonir <khanmonir1335@gmail.com>
2022-05-22 22:07:30 +0600

    wrapping everything into context provider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
+import { ContextProvider } from './context/ContextProvider';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
