1f7494a9f6b93af68588d16ad40a33a2f926ab44
MkMonir <khanmonir1335@gmail.com>
2022-05-23 20:00:59 +0600

    add isclicked and screensize functionality

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -2,18 +2,36 @@ import { createContext, useContext, useState } from 'react';
 
 const StateContext = createContext();
 
-const initialContext = {
+const initialState = {
   chat: false,
   cart: false,
   userProfile: false,
-  notifications: false,
+  notification: false,
 };
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true });
+  };
 
   return (
-    <StateContext.Provider value={{ activeMenu, setActiveMenu }}>{children}</StateContext.Provider>
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
   );
 };
 
